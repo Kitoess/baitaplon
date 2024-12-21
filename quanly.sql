beeae9227190ae77587009e810ab408ad949c71b
Nguyễn Nam Khánh <nammkhanhnguyen@gmail.com>
2024-12-21 13:12:00 +0700

    sql

--- quanly.sql
+++ quanly.sql
@@ -32,7 +32,7 @@ USE `quanly`;
 CREATE TABLE `khachhang` (
   `id` int(11) NOT NULL,
   `fullname` varchar(50) NOT NULL,
-  `location` varchar(100) NOT NULL,
+  `address` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `number` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
